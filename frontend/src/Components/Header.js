a0d9f85fa424bb2ff2aae4f8a64720efb2bcd5a4
Eugene Goh <gzkgt11@gmail.com>
2021-03-31 11:06:58 +0800

    Added dark mode

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,27 +1,46 @@
-import React from 'react'
-import '../App.css';
-import { Link } from 'react-router-dom'
-
+import React, { useState } from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./GlobalStyle";
+import { darkTheme, lightTheme } from "./Theme";
+import { CgDarkMode } from "react-icons/cg";
 
 function Header() {
-    return (
-        <div id="header">
+  const [theme, setTheme] = useState("light");
 
-            <div id="logo">
-                <Link to={'/'}>
-                    <h3>MUMBLE</h3>
-                </Link>
-            </div>
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
 
-            <div id="nav-wrapper">
-                <i className="fas fa-bell nav-item nav-icon"></i>
-                {/* This will eventually be drop down list with options like settings, porfile, logout, etc */}
-                <Link to={'/settings'}>
-                    <img alt="img-description" className="user-thumbnail user-thumbnail-sm nav-item" src="https://randomuser.me/api/portraits/men/52.jpg" />
-                </Link>
-            </div>
+  return (
+    <div id="header">
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <GlobalStyles />
+        <div id="logo">
+          <Link to={"/"}>
+            <h3>MUMBLE</h3>
+          </Link>
         </div>
-    )
+
+        <div id="nav-wrapper">
+          <i className="fas fa-bell nav-item nav-icon"></i>
+          {/* This will eventually be drop down list with options like settings, porfile, logout, etc */}
+          <Link to={"/settings"}>
+            <img
+              alt="img-description"
+              className="user-thumbnail user-thumbnail-sm nav-item"
+              src="https://randomuser.me/api/portraits/men/52.jpg"
+            />
+          </Link>
+
+          <button className="btn-toggle">
+            <CgDarkMode onClick={themeToggler} size="3em" color="#ffffff" />
+          </button>
+        </div>
+      </ThemeProvider>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
---
+++ frontend/src/Components/Theme.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  body: "#FFF",
+  text: "#363537",
+  toggleBorder: "#FFF",
+  background: "#363537",
+};
+
+export const darkTheme = {
+  body: "#363537",
+  text: "#1b2021",
+  toggleBorder: "#6B8096",
+  background: "#999",
+};
