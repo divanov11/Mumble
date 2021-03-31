393463f5555be0d84548e7c7390342e588099c26
mani-barathi <manibarathi.s@gmail.com>
2021-03-31 09:34:04 +0530

    cleaned up App.js & added user state to ease up the dev process

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import "./App.css";
 import { HashRouter as Router, Route } from "react-router-dom";
 
@@ -10,17 +11,26 @@ import UserSettingsPage from "./Pages/UserSettingsPage";
 import UserSearchPage from "./Pages/UserSearchPage";
 
 function App() {
+  // change the user state to null to see the login page 
+  const [user, setUser] = useState('john')
+
   return (
     <Router>
-      <Header />
-      <Route component={LoginPage} path={"/login"} />
-      <main>
-        <Route component={HomePage} path={"/"} exact />
-        <Route component={ProfilePage} path={"/profile/:username"} />
-        <Route component={UserSettingsPage} path={"/settings"} />
-        <Route component={Discussion} path={"/discussion/:slug"} />
-        <Route component={UserSearchPage} path={"/find-user"} />
-      </main>
+      {!user ? (           // if no user is Logged in show the LoginPage 
+        <LoginPage />
+      ) : (               // else show the App
+        <>
+          <Header />
+          <main>
+            <Route component={HomePage} path={"/"} exact />
+            <Route component={ProfilePage} path={"/profile/:username"} />
+            <Route component={UserSettingsPage} path={"/settings"} />
+            <Route component={Discussion} path={"/discussion/:slug"} />
+            <Route component={UserSearchPage} path={"/find-user"} />
+          </main>
+
+        </>
+      )}
     </Router>
   );
 }
