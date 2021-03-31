f06aa15bb265cf291ec1283b5433f7b2f5190900
Isaiah <isaiah.abiodun@outlook.com>
2021-03-31 02:44:01 -0400

    usersettings reposiveness

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -1,60 +1,77 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import '../Css/login.css';
-import screenshot from '../Images/screenshot.PNG'
+import screenshot from '../Images/screenshot.PNG';
 
 function LoginPage() {
     return (
-        <div id="login-page-container">
-            <div id="left-column">
-                <div id="left-column-content">
-                    <h1 id="headline">A place for developers to</h1>
-                    <p id="subheadline">Share Projects - Ask Questions - Have Discussions - Write Articles</p>
-                    <img alt="img-description" id="screenshot" src={screenshot} />
-                    <p><small>An open source project. For the community, by the community</small></p>
+        <div id='login-page-container'>
+            <div id='left-column'>
+                <div id='left-column-content'>
+                    <h1 id='headline'>A place for developers to</h1>
+                    <p id='subheadline'>
+                        Share Projects - Ask Questions - Have Discussions -
+                        Write Articles
+                    </p>
+                    <img
+                        alt='img-description'
+                        id='screenshot'
+                        src={screenshot}
+                    />
+                    <p>
+                        <small>
+                            An open source project. For the community, by the
+                            community
+                        </small>
+                    </p>
                 </div>
-
             </div>
 
-            <div id="right-column">
-
-                <div id="form-wrapper">
-                    <h1 id="title">Mumble</h1>
-                    <form action="#" class="form">
-
-                        <div class="form__field">
-                            <label for="formInput#email">Email: </label>
+            <div id='right-column'>
+                <div id='form-wrapper'>
+                    <h1 id='title'>Mumble</h1>
+                    <form action='#' class='form'>
+                        <div class='form__field'>
+                            <label for='formInput#email'>Email: </label>
                             <input
-                                class="input input--email"
-                                id="formInput#email"
-                                type="email"
-                                name="email"
-                                placeholder="e.g. user@domain.com"
+                                class='input input--email'
+                                id='formInput#email'
+                                type='email'
+                                name='email'
+                                placeholder='e.g. user@domain.com'
                             />
                         </div>
 
-                        <div class="form__field">
-                            <label for="formInput#password">Password: </label>
+                        <div class='form__field'>
+                            <label for='formInput#password'>Password: </label>
                             <input
-                                class="input input--password"
-                                id="formInput#passowrd"
-                                type="password"
-                                name="password"
-                                placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
+                                class='input input--password'
+                                id='formInput#passowrd'
+                                type='password'
+                                name='password'
+                                placeholder='&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;'
                             />
                         </div>
 
-
-                        <input className="submit" type="submit" value="Sign In" />
+                        <input
+                            className='submit'
+                            type='submit'
+                            value='Sign In'
+                        />
                     </form>
-                    <div id="bottom-content">
-
-                        <p>Already have an account? <a href="#">Register</a></p>
-                        <p><a href="#">Forgot Password?</a></p>
+                    <div id='bottom-content'>
+                        <p>
+                            Already have an account?{' '}
+                            <Link to='#'>Register</Link>
+                        </p>
+                        <p>
+                            <Link to='#'>Forgot Password?</Link>
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default LoginPage
+export default LoginPage;
--- frontend/src/Pages/UserSettingsPage.js
+++ frontend/src/Pages/UserSettingsPage.js
@@ -1,92 +1,102 @@
-import React from 'react'
-import '../Css/UserSettings.css'
+import React from 'react';
+import '../Css/UserSettings.css';
+import { Link } from 'react-router-dom';
 
 function UserSettingsPage() {
     return (
-        <div id="settings-page-container">
+        <div id='settings-page-container'>
             <section>
-                <div className="card">
-                    <div className="card-body">
+                <div className='card'>
+                    <div className='card-body'>
                         <h6>Account Settings</h6>
-                        <div className="line-break"></div>
-                        <div id="settings-pic-wrapper">
-                            <img alt="img-description" className="user-thumbnail user-thumbnail-lg" src="https://randomuser.me/api/portraits/men/52.jpg" />
+                        <div className='line-break'></div>
+                        <div id='settings-pic-wrapper'>
+                            <img
+                                alt='img-description'
+                                className='user-thumbnail user-thumbnail-lg'
+                                src='https://randomuser.me/api/portraits/men/52.jpg'
+                            />
                             <h5>Sam Wick</h5>
-                            <div className="line-break"></div>
-                            <a className="btn btn-1 btn-sm">Update Picture</a>
+                            <div className='line-break'></div>
+                            <Link to='' className='btn btn-1 btn-sm'>
+                                Update Picture
+                            </Link>
                         </div>
                     </div>
                 </div>
             </section>
             <section>
-                <div className="card">
-                    <div className="card-body">
-
-                        <div className="card card-dark">
-                            <div className="card-body">
-                                <h6>User Information <a href="#">Edit</a></h6>
-                                <div className="line-break line-break-2"></div>
+                <div className='card'>
+                    <div className='card-body'>
+                        <div className='card card-dark'>
+                            <div className='card-body'>
+                                <h6>
+                                    User Information <Link to=''>Edit</Link>
+                                </h6>
+                                <div className='line-break line-break-2'></div>
                                 <p>Name: Sam Wick</p>
-                                <p>Bio: I was a lead developer in a past life, now I enjoy teaching others how to build cool applications.</p>
+                                <p>
+                                    Bio: I was a lead developer in a past life,
+                                    now I enjoy teaching others how to build
+                                    cool applications.
+                                </p>
                                 <p>Location: Sam Wick</p>
                             </div>
                         </div>
 
-                        <div className="card card-dark">
-                            <div className="card-body">
-                                <h6>Username <a href="#">Edit</a></h6>
-                                <div className="line-break line-break-2"></div>
+                        <div className='card card-dark'>
+                            <div className='card-body'>
+                                <h6>
+                                    Username <Link href='#'>Edit</Link>
+                                </h6>
+                                <div className='line-break line-break-2'></div>
                                 <p>Username: @realsamwick</p>
                             </div>
                         </div>
 
-
-                        <div className="card card-dark">
-                            <div className="card-body">
-
-                                <h6>Skills  <a href="#">Edit</a></h6>
-                                <div className="line-break line-break-2"></div>
-                                <div id="topics-wrapper" className="card-body">
-
-                                    <div className="tags-wrapper">
-                                        <div className="tag">
+                        <div className='card card-dark'>
+                            <div className='card-body'>
+                                <h6>
+                                    Skills <Link href='#'>Edit</Link>
+                                </h6>
+                                <div className='line-break line-break-2'></div>
+                                <div id='topics-wrapper' className='card-body'>
+                                    <div className='tags-wrapper'>
+                                        <div className='tag'>
                                             <small>Python</small>
                                         </div>
-                                        <div className="tag">
+                                        <div className='tag'>
                                             <small>Django</small>
                                         </div>
-                                        <div className="tag">
+                                        <div className='tag'>
                                             <small>React</small>
                                         </div>
-                                        <div className="tag">
+                                        <div className='tag'>
                                             <small>JavaScript</small>
                                         </div>
-                                        <div className="tag">
+                                        <div className='tag'>
                                             <small>Node JS</small>
                                         </div>
-                                        <div className="tag">
+                                        <div className='tag'>
                                             <small>Postgres</small>
                                         </div>
-                                        <div className="tag">
+                                        <div className='tag'>
                                             <small>System Architecture</small>
                                         </div>
-                                        <div className="tag">
+                                        <div className='tag'>
                                             <small>Security</small>
                                         </div>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </section>
 
-
             <section></section>
-        </div >
-    )
+        </div>
+    );
 }
 
-export default UserSettingsPage
+export default UserSettingsPage;
