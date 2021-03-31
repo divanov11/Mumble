895a34e046603a11425128c80b58ebf90db26e02
Guillermo Brachetta <brachetta@me.com>
2021-03-31 23:35:05 +0200

    replace string id by num, as HomePage checks for equality (===) - fix typo in "contributers" (contributors)

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,44 +1,44 @@
-import React from 'react';
+import React from "react";
 // import { Link } from 'react-router-dom';
 
 //Components
-import Contributors from '../Components/Contributors';
-import Feed from '../Components/Feed';
-import PostForm from '../Components/PostForm';
-import TopicTags from '../Components/TopicTags';
-import DiscussionsCard from '../Components/DiscussionsCard';
-import ArticlesCard from '../Components/ArticlesCard';
+import Contributors from "../Components/Contributors";
+import Feed from "../Components/Feed";
+import PostForm from "../Components/PostForm";
+import TopicTags from "../Components/TopicTags";
+import DiscussionsCard from "../Components/DiscussionsCard";
+import ArticlesCard from "../Components/ArticlesCard";
 
 //Dummy Data Files
-import postsData from '../data/posts';
-import userData from '../data/users';
-import discussions from '../data/discussions';
-import articles from '../data/articles';
+import postsData from "../data/posts";
+import userData from "../data/users";
+import discussions from "../data/discussions";
+import articles from "../data/articles";
 
 function HomePage() {
-    let posts = postsData;
-    let user = userData.find((u) => u.id === 1);
-    let contributers = userData;
+  let posts = postsData;
+  let user = userData.find((u) => u.id === 1);
+  let contributors = userData;
 
-    return (
-        <div id='content-container'>
-            <section id='left-sidebar'>
-                <Contributors users={contributers} />
-                {/* User user skills as placeholder but this should be Topics a user follows */}
-                <TopicTags tags={user.interests} />
-            </section>
+  return (
+    <div id="content-container">
+      <section id="left-sidebar">
+        <Contributors users={contributors} />
+        {/* User user skills as placeholder but this should be Topics a user follows */}
+        <TopicTags tags={user.interests} />
+      </section>
 
-            <section id='center-content'>
-                <PostForm />
-                <Feed posts={posts} />
-            </section>
+      <section id="center-content">
+        <PostForm />
+        <Feed posts={posts} />
+      </section>
 
-            <section id='right-sidebar'>
-                <DiscussionsCard discussions={discussions} />
-                <ArticlesCard articles={articles} />
-            </section>
-        </div>
-    );
+      <section id="right-sidebar">
+        <DiscussionsCard discussions={discussions} />
+        <ArticlesCard articles={articles} />
+      </section>
+    </div>
+  );
 }
 
 export default HomePage;
