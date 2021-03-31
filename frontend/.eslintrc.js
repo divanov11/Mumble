b87aef686e2a39ed426d56f64c8cafc7fa1ad9f5
Zach Lefkovitz <53406696+i-am-zach@users.noreply.github.com>
2021-03-30 23:55:36 -0500

    Minify .eslintrc.js to extend react-app

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,25 +1,6 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
-  },
   extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    'react-app',
     'plugin:prettier/recommended',
   ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react'],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
 }
