87f470be5b09dcef69a544876a95d0ae62fe4f2f
Isaiah Abiodun <38655623+abisalde@users.noreply.github.com>
2021-03-31 14:25:26 +0100

    usersettings reposiveness (#54)
    Co-authored-by: Dennis Ivy <33843378+divanov11@users.noreply.github.com>

--- frontend/src/Pages/Discussion.js
+++ frontend/src/Pages/Discussion.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Contributors from '../Components/Contributors'
-import DiscussionsCard from '../Components/DiscussionsCard'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Contributors from '../Components/Contributors';
+import DiscussionsCard from '../Components/DiscussionsCard';
 
 import '../Css/Discussion.css';
 import userData from '../data/users'
 import discussions from '../data/discussions'
 import VotingWidget from '../Components/VotingWidget'
 
+
 function Discussion({ match }) {
-    let discussion = discussions.find((d) => d.slug == match.params.slug)
-    let relatedQuestions = discussions.filter((d) => d.slug != match.params.slug)
+    let discussion = discussions.find((d) => d.slug === match.params.slug);
+    let relatedQuestions = discussions.filter(
+        (d) => d.slug !== match.params.slug
+    );
 
     return (
-        <div id="discussion-container">
+        <div id='discussion-container'>
             <section>
                 <div className="card">
                     <div className="card-body">
@@ -24,23 +27,32 @@ function Discussion({ match }) {
                                 <div className="custom-spacer"></div>
                                 <VotingWidget voteRatio={discussion.vote_ratio}/>
                             </div>
-                            <div className="question-body">
-                                <h1 className="discussion-headline">{discussion.headline}</h1>
-                                <p className="asked-by">Asked by <Link to={`/profile/${discussion.user.username}`}>{discussion.user.name}</Link> {discussion.created}</p>
+                            <div className='question-body'>
+                                <h1 className='discussion-headline'>
+                                    {discussion.headline}
+                                </h1>
+                                <p className='asked-by'>
+                                    Asked by{' '}
+                                    <Link
+                                        to={`/profile/${discussion.user.username}`}
+                                    >
+                                        {discussion.user.name}
+                                    </Link>{' '}
+                                    {discussion.created}
+                                </p>
 
-                                <div className="line-break"></div>
+                                <div className='line-break'></div>
 
-                                <div className="tags-wrapper">
-                                    {discussion.tags.map(tag => (
-                                        <div className="tag">
+                                <div className='tags-wrapper'>
+                                    {discussion.tags.map((tag) => (
+                                        <div className='tag'>
                                             <small>{tag}</small>
                                         </div>
                                     ))}
-
                                 </div>
-                                <div className="line-break"></div>
+                                <div className='line-break'></div>
                                 <p>{discussion.body}</p>
-                                <div className="custom-spacer"></div>
+                                <div className='custom-spacer'></div>
                                 <h6>3 Answers</h6>
                                 <div className="line-break"></div>
 
@@ -52,30 +64,42 @@ function Discussion({ match }) {
                                             {/* <div className="custom-spacer"></div> */}
                                             <VotingWidget voteRatio={answer.vote_ratio}/>
                                         </div>
-                                        <div className="question-body">
-                                            <Link to={`/profile/${answer.user.username}`}><h6>{answer.user.name} <small><i> @{answer.user.username}</i></small></h6>
+                                        <div className='question-body'>
+                                            <Link
+                                                to={`/profile/${answer.user.username}`}
+                                            >
+                                                <h6>
+                                                    {answer.user.name}{' '}
+                                                    <small>
+                                                        <i>
+                                                            {' '}
+                                                            @
+                                                            {
+                                                                answer.user
+                                                                    .username
+                                                            }
+                                                        </i>
+                                                    </small>
+                                                </h6>
                                             </Link>
                                             <p>{answer.content}</p>
-                                            {/* <div className="custom-spacer"></div> */}
-                                            {/* <div className="line-break"></div> */}
+                                            <div className='custom-spacer'></div>
+                                            <div className='line-break'></div>
+
                                         </div>
                                     </div>
                                 ))}
                             </div>
                         </div>
-
                     </div>
                 </div>
-
-
             </section>
-            <section id="right-sidebar">
+            <section id='right-sidebar'>
                 <Contributors users={userData} />
                 <DiscussionsCard discussions={relatedQuestions} />
             </section>
-
         </div>
-    )
+    );
 }
 
-export default Discussion
+export default Discussion;
