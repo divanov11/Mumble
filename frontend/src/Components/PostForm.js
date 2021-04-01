85769516c59053043eb705180364d29d734e730f
Zach Lefkovitz <53406696+i-am-zach@users.noreply.github.com>
2021-04-01 00:47:33 -0500

    Basic quality of life changes to home page. (#71)

--- frontend/src/Components/PostForm.js
+++ frontend/src/Components/PostForm.js
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function PostForm() {
-    return (
-        <div className="card">
-            <div className="card__body">
+  const [message, setMessage] = useState("");
 
-                <form id="feed-post-form" method="POST">
-                    <textarea placeholder="Share your brilliant thought!"></textarea>
-                    <div id="post-btn-wrapper">
-                        <input id="post-btn" className="btn btn--main" type="submit" value="Post" />
-                    </div>
+  /**
+   * Function that is called when post form is submitted
+   */
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    // Do something with the form data
+    alert(`Creating new post with message: ${message}`);
+    // Clear the form fields
+    setMessage("");
+  };
 
-                </form>
-            </div>
-        </div>
+  /**
+   * Function that updates the message hook to the form value
+   */
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
 
-
-    )
+  return (
+    <div className="card">
+      <div className="card__body">
+        <h3>Create Post</h3>
+        <form className="form" onSubmit={onFormSubmit}>
+          <div className="form__field">
+            <textarea
+              className="input input--textarea"
+              name="message"
+              id="message"
+              placeholder="Share your brilliant thought!"
+              onChange={handleMessageChange}
+              value={message}
+            ></textarea>
+            <button id="post-btn" className="btn btn--main" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default PostForm
+export default PostForm;
