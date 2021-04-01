45e2865692ac664d434f324584ca2327c367d381
agentraghav <raghvendrarathore2@gmail.com>
2021-04-01 18:49:20 +0530

    used prettier

--- frontend/src/data/articles.js
+++ frontend/src/data/articles.js
@@ -1,25 +1,25 @@
 const articles = [
-    {
-        'id': '1',
-        'title': 'What are Django class based views & should you use them?',
-        'vote_rank': '132',
-        'body': '',
-        'slug': '',
-    },
-    {
-        'id': '2',
-        'title': 'WordPress Vs Hand Coding',
-        'vote_rank': '116',
-        'body': '',
-        'slug': '',
-    },
-    {
-        'id': '3',
-        'title': '5 things that ranked my website fast',
-        'vote_rank': '54',
-        'body': '',
-        'slug': '',
-    },
-]
+  {
+    id: "1",
+    title: "What are Django class based views & should you use them?",
+    vote_rank: "132",
+    body: "",
+    slug: "",
+  },
+  {
+    id: "2",
+    title: "WordPress Vs Hand Coding",
+    vote_rank: "116",
+    body: "",
+    slug: "",
+  },
+  {
+    id: "3",
+    title: "5 things that ranked my website fast",
+    vote_rank: "54",
+    body: "",
+    slug: "",
+  },
+];
 
 export default articles;
