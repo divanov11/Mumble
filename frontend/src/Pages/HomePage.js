693d51a2e2a90c23fe3f961623737e3b7d420e50
Guillermo Brachetta <57924268+GBrachetta@users.noreply.github.com>
2021-04-01 02:22:12 +0200

    set up prettier to avoid unnecessary merge conflicts - fix users.js to have numeric id, or app breaks on launch (#69)
    * set up prettier to avoid unnecessary merge conflicts
    
    * replace string id by num, as HomePage checks for equality (===) - fix typo in "contributers" (contributors)
    
    Co-authored-by: Dennis Ivy <33843378+divanov11@users.noreply.github.com>

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,20 +1,22 @@
+
 import React from 'react';
 import '../Css/HomePage.css'
 //Components
-import Contributors from '../Components/Contributors';
-import Feed from '../Components/Feed';
-import PostForm from '../Components/PostForm';
-import TopicTags from '../Components/TopicTags';
-import DiscussionsCard from '../Components/DiscussionsCard';
-import ArticlesCard from '../Components/ArticlesCard';
+import Contributors from "../Components/Contributors";
+import Feed from "../Components/Feed";
+import PostForm from "../Components/PostForm";
+import TopicTags from "../Components/TopicTags";
+import DiscussionsCard from "../Components/DiscussionsCard";
+import ArticlesCard from "../Components/ArticlesCard";
 
 //Dummy Data Files
-import postsData from '../data/posts';
-import userData from '../data/users';
-import discussions from '../data/discussions';
-import articles from '../data/articles';
+import postsData from "../data/posts";
+import userData from "../data/users";
+import discussions from "../data/discussions";
+import articles from "../data/articles";
 
 function HomePage() {
+
     let posts = postsData;
     let user = userData.find((u) => u.id === '1');
     let contributers = userData;
@@ -26,10 +28,11 @@ function HomePage() {
                 <TopicTags tags={user.interests} />
             </section>
 
-            <section id='center-content'>
-                <PostForm />
-                <Feed posts={posts} />
-            </section>
+      <section id="center-content">
+        <PostForm />
+        <Feed posts={posts} />
+      </section>
+
 
             <section id='sidebar--right--home'>
                 <DiscussionsCard discussions={discussions} />
