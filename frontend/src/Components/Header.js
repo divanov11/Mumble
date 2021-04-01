42cc9028d8216a78a5ccd877aa43eba5835cfa56
AFzal <saiyedafzalgz@gmail.com>
2021-04-01 15:42:20 -0700

    code formatted

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,15 +1,14 @@
-
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import userData from "../data/users";
-import { useDetectClickOutside } from "react-detect-click-outside";
-import { useHistory } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import userData from '../data/users';
+import { useDetectClickOutside } from 'react-detect-click-outside';
+import { useHistory } from 'react-router-dom';
 import '../styles/components/HeaderBar.css';
 
 function Header() {
   const user = userData.find((u) => Number(u.id) === 1);
   const [showNavigation, setShowNavigation] = useState(false);
-  const history = useHistory()
+  const history = useHistory();
   const toggleDropdown = (e) => {
     e.stopPropagation();
     setShowNavigation(!showNavigation);
@@ -42,37 +41,40 @@ function Header() {
 
       {showNavigation && (
         <div ref={navigationRef} className="card" id="user--navigation">
-          <div className="user-navigation--item" onClick={(e) => {
-              e.stopPropagation()
-              closeDropdown()
-              history.push(`/profile/${user.username}`)
-              }}>
+          <div
+            className="user-navigation--item"
+            onClick={(e) => {
+              e.stopPropagation();
+              closeDropdown();
+              history.push(`/profile/${user.username}`);
+            }}
+          >
             <i className="fas fa-user user--nav--icon"></i>
-            <Link to={`/profile/${user.username}`}>
-              Profile
-            </Link>
+            <Link to={`/profile/${user.username}`}>Profile</Link>
           </div>
 
-          <div className="user-navigation--item" onClick={(e) => {
-              e.stopPropagation()
-              closeDropdown()
-              history.push("/settings")
-              }}>
+          <div
+            className="user-navigation--item"
+            onClick={(e) => {
+              e.stopPropagation();
+              closeDropdown();
+              history.push('/settings');
+            }}
+          >
             <i className="fas fa-cog user--nav--icon"></i>
-            <Link to={"/settings"}>
-              Settings
-            </Link>
+            <Link to={'/settings'}>Settings</Link>
           </div>
 
-          <div className="user-navigation--item" onClick={(e) => {
-            e.stopPropagation()
-            closeDropdown()
-            history.push("/logout")
-            }}>
+          <div
+            className="user-navigation--item"
+            onClick={(e) => {
+              e.stopPropagation();
+              closeDropdown();
+              history.push('/logout');
+            }}
+          >
             <i className="fas fa-sign-out-alt user--nav--icon"></i>
-            <Link to={"/logout"}>
-              Logout
-            </Link>
+            <Link to={'/logout'}>Logout</Link>
           </div>
         </div>
       )}
