639f00cdc5244b24c3a6cf3522e0eada55c06d20
Shahriar Parvez <mr.spshuvo@gmail.com>
2021-04-02 06:04:35 +0600

    :star2: improve codes for header navbar dropdown

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import userData from '../data/users';
 import { useDetectClickOutside } from 'react-detect-click-outside';
-import { useHistory } from 'react-router-dom';
+
+import userData from '../data/users';
 import '../styles/components/HeaderBar.css';
 
 function Header() {
   const user = userData.find((u) => Number(u.id) === 1);
   const [showNavigation, setShowNavigation] = useState(false);
-  const history = useHistory();
+
   const toggleDropdown = (e) => {
     e.stopPropagation();
     setShowNavigation(!showNavigation);
   };
 
-  const closeDropdown = (e) => {
-    setShowNavigation(false);
-  };
+  const closeDropdown = () => setShowNavigation(false);
 
   const navigationRef = useDetectClickOutside({
     onTriggered: closeDropdown,
@@ -43,38 +41,32 @@ function Header() {
 
       {showNavigation && (
         <div ref={navigationRef} className="card" id="user--navigation">
-          <div
+          <Link
+            to={`/profile/${user.username}`}
             className="user-navigation--item"
-            onClick={(e) => {
-              closeDropdown(e);
-              history.push(`/profile/${user.username}`);
-            }}
+            onClick={closeDropdown}
           >
             <i className="fas fa-user user--nav--icon"></i>
-            <Link to={`/profile/${user.username}`}>Profile</Link>
-          </div>
+            Profile
+          </Link>
 
-          <div
+          <Link
+            to="/settings"
             className="user-navigation--item"
-            onClick={(e) => {
-              closeDropdown(e);
-              history.push('/settings');
-            }}
+            onClick={closeDropdown}
           >
             <i className="fas fa-cog user--nav--icon"></i>
-            <Link to={'/settings'}>Settings</Link>
-          </div>
+            Settings
+          </Link>
 
-          <div
+          <Link
+            to="/logout"
             className="user-navigation--item"
-            onClick={(e) => {
-              closeDropdown(e);
-              history.push('/logout');
-            }}
+            onClick={closeDropdown}
           >
             <i className="fas fa-sign-out-alt user--nav--icon"></i>
-            <Link to={'/logout'}>Logout</Link>
-          </div>
+            Logout
+          </Link>
         </div>
       )}
     </div>
