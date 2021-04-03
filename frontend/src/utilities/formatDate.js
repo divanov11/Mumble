9b8b8eaefb2e5441a6a0e573cc0656d5ae9bb1c2
Shahriar Parvez <mr.spshuvo@gmail.com>
2021-04-03 06:02:44 +0600

    :calender: formate date with date-fns

---
+++ frontend/src/utilities/formatDate.js
@@ -0,0 +1,6 @@
+import { formatDistanceToNow } from 'date-fns';
+
+export const sqlDateToJsDate = (date) => new Date(date.replace(' ', ''));
+
+export const distanceDate = (date) =>
+  formatDistanceToNow(sqlDateToJsDate(date), { addSuffix: true });
