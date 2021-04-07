882d25dbf315bd475c85b55d025fb37f7013358d
divanov11 <ivanovsin11@gmail.com>
2021-04-07 13:43:13 -0500

    Set up base redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import store from './store'
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './styles/index.css';
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,17 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+const reducer = combineReducers({});
 
-const reducer = combineReducers({})
+const initialState = {};
 
+const middleWare = [thunk];
 
-const initialState = {}
-
-const middleWare = [thunk]
-
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleWare)))
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleWare)),
+);
 
 export default store;
