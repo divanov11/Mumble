d5540e3d313712a62aacbb991954f95c7a163938
jewells07 <jewellsjoshi437@gmail.com>
2021-04-10 18:37:32 +0530

    fix: localstorage of mumble theme

--- frontend/src/reducers/local.js
+++ frontend/src/reducers/local.js
@@ -1,12 +1,18 @@
 import { MUMBLETHEME } from '../constants/localConstants';
 
+// localStorage store as a string, So this will convert into boolean
+const isDarkTheme = localStorage.getItem('mumble-theme') === 'true' ? true : false;
+const isNull = localStorage.getItem('mumble-theme') === null;
+
 const initialState = {
-  darkTheme: false,
+  darkTheme: isNull ? false : isDarkTheme,
 };
 
 export default function localReducer(state = initialState, action) {
-  switch (action.type) {
+  const { type } = action;
+  switch (type) {
     case MUMBLETHEME:
+      localStorage.setItem('mumble-theme', !state.darkTheme);
       return {
         ...state,
         darkTheme: !state.darkTheme,
