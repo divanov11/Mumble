51f1c0a33ab4ff7b630107de27f2c1155d71f5af
Hisham-Pak <hishammahmood41@gmail.com>
2021-04-10 17:10:01 +0500

    Add WYSIWYG (CKEditor) in create article page #215

--- frontend/src/pages/CreateArticlePage.js
+++ frontend/src/pages/CreateArticlePage.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 import '../styles/components/CreateArticlePage.css';
 
-import { Button, Input, TextArea } from '../common';
+import { Button, Input } from '../common';
 import { ArticlesCard } from '../components';
 import { articles } from '../data';
 
 const CreateArticlePage = () => {
   const history = useHistory();
   const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
+  const [body, setBody] = useState('Share your mumble thoughts...');
 
   const handleTitleChange = (e) => setTitle(e.target.value);
-  const handleBodyChange = (e) => setBody(e.target.value);
+  const handleBodyChange = (e, editor) => {
+    const data = editor.getData();
+    console.log(data);
+    setBody(data);
+  };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
     // use the form data and make a request to API
-    alert('Article Created!! \n Now you will be directed to the Articles Page');
+    alert('Article Created! \n Now you will be directed to the Articles Page');
     // redirect to the articles page, in the real request slug should be changed to created article's slug
     history.push(`/article/article1`);
   };
@@ -40,7 +47,22 @@ const CreateArticlePage = () => {
                 label="Title"
                 required
               />
-              <TextArea value={body} onChange={handleBodyChange} required />
+              <CKEditor
+                editor={ClassicEditor}
+                data={body}
+                onChange={handleBodyChange}
+                onFocus={(event, editor) => {
+                  setBody((prevState) => {
+                    // Strips html tags from prevState
+                    // So gets blank even if user clicked any ckeditor option (list, quotes, etc.)
+                    // before clicking content area
+                    if (prevState.replace(/<[^>]*>?/gm, '') === 'Share your mumble thoughts...') {
+                      return '';
+                    }
+                    return prevState;
+                  });
+                }}
+              />
               <Button color="main" type="submit">
                 Submit
               </Button>
