5c584b31104d18eee99d568f515ff03de48beb0e
Mohammad Khaled <eng_mohammadkhaled@yahoo.com>
2021-04-14 22:40:55 +0200

    add dropdowmn menu to posts

--- frontend/src/common/PostCard.js
+++ frontend/src/common/PostCard.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getApiUrl } from '../services/config';
-
+import { useDetectClickOutside } from 'react-detect-click-outside';
 import AuthorBox from './AuthorBox';
 import PostAction from './PostAction';
 import VotingWidget from './VotingWidget';
@@ -21,14 +21,41 @@ const PostCard = ({ post, link, isComment = false, children, ...others }) => {
 
   let dispatch = useDispatch();
 
+  const auth = useSelector((state) => state.auth);
+
+  //user.id = auth.user.id;
+
   let [comments, setComments] = useState([]);
 
   const [showComments, setShowComments] = useState(false);
+
+  const [showPostMenu, setShowPostMenu] = useState(false);
 
   const toggleComments = () => {
     setShowComments(!showComments);
     dispatch(getPostComments(setComments, post.id));
   };
+
+  const handlePostDropdownMenu = () => {
+    setShowPostMenu(prevState => !prevState);
+  };
+
+  const closePostMenu = (e) => {
+    if (navigationRef.current.classList.contains('dropmenu--show')) {
+      navigationRef.current.classList.remove('dropmenu--show');
+      setShowPostMenu(false);
+    }
+  }
+
+  const navigationRef = useDetectClickOutside({
+    onTriggered: closePostMenu,
+  });
+
+  useEffect(() => {
+    if(showPostMenu) {
+      navigationRef.current.classList.toggle('dropmenu--show');
+    }
+  }, [navigationRef, showPostMenu]);
 
   return (
     <div className={`${isComment && 'post-card--comment'}`} {...others}>
@@ -40,7 +67,36 @@ const PostCard = ({ post, link, isComment = false, children, ...others }) => {
           url={`/profile/${user.username}`}
           size={isComment ? 'sm' : 'md'}
         />
-        <p className="post-meta">{formatDate.distanceDate(created)}</p>
+        <div className="post--options">
+          <div className="post__dropmenu">
+            <p className="post__dropmenu--icon" onClick={handlePostDropdownMenu}>
+              ...
+            </p>
+            <div ref={navigationRef} className="card dropmenu">
+            {auth.user.id === user.id &&
+            <>
+              <div role="button" className="dropmenu__menuitem">
+                <i className="fas fa-edit post__dropmenu--navicon"></i>
+                Edit
+              </div>
+              <div role="button" className="dropmenu__menuitem">
+                <i className="fas fa-trash post__dropmenu--navicon"></i>
+                Delete
+              </div>
+            </>
+            }
+            {auth.user.id !== user.id &&
+            <>
+              <div role="button" className="dropmenu__menuitem">
+                <i className="fas fa-minus-circle post__dropmenu--navicon"></i>
+                Hide Post
+              </div>
+            </>
+            }
+            </div>
+          </div>
+          <p className="post-meta">{formatDate.distanceDate(created)}</p>
+        </div>
       </div>
       <div className="post-contents">
         <VotingWidget votes={vote_rank} />
