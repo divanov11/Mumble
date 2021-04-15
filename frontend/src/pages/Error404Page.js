9f23d127e865cf2b96d9923dda9be6d77fa20ca3
MidouWebDev <72341453+MidouWebDev@users.noreply.github.com>
2021-04-15 16:39:31 +0000

    Added "Alt" to "img" (thanks @itzomen 😁)
    Trying to fix the error of checks

--- frontend/src/pages/Error404Page.js
+++ frontend/src/pages/Error404Page.js
@@ -5,7 +5,7 @@ import '../styles/components/Error404Page.css';
 
 const Error404Page = () => (
   <div className="main404page">
-    <h1>4<span><img src="./frontend/public/apple-touch-icon.png"/></span>4</h1>
+    <h1>4<span><img src="./frontend/public/apple-touch-icon.png" alt="Mumble Logo"/></span>4</h1>
     <h2>Page Not Found !</h2>
     <br/>
     <p>Looks like you got lost</p>
