2994a87d5622c51724e074b5a98b873481816bda
MidouWebDev <72341453+MidouWebDev@users.noreply.github.com>
2021-04-15 17:44:31 +0000

    Updated a tag in Error500Page.js

--- frontend/src/pages/Error500Page.js
+++ frontend/src/pages/Error500Page.js
@@ -7,7 +7,7 @@ import '../styles/components/Error500Page.css';
 const Error500Page = ({ error }) => (
   <div className="main500page">
     <h1>5<span><img src="https://mumble.dev/apple-touch-icon.png" alt="Mumble Logo (0)"/></span><span><img src="https://mumble.dev/apple-touch-icon.png" alt="Mumble Logo (0)"/></span></h1>
-    <h1>🚨 - Internal Error !</h1>
+    <h2>🚨 - Internal Error !</h2>
     <p>Something went wrong</p>
     <br/>
     <Link to="/">Click here</Link> to go back to the 🏡 home page.
