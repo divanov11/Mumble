0bab70ea0f98a7a1b85afb5966bc3335987210dd
Cody Seibert <cseibert@flexion.us>
2021-04-17 23:17:07 -0400

    fixing the 404 and 500 page BEM styles

--- frontend/src/pages/Error500Page.js
+++ frontend/src/pages/Error500Page.js
@@ -5,29 +5,22 @@ import '../styles/components/Error500Page.css';
 
 const Error500Page = ({ error }) => (
   <div className="main500page">
-    <h1>
+    <h1 className="main500page__banner">
       5
-      <span>
-        <img
-          className="image__500"
-          src="https://mumble.dev/apple-touch-icon.png"
-          alt="Mumble Logo (0)"
-        />
-      </span>
-      <span>
-        <img
-          className="image__500"
-          src="https://mumble.dev/apple-touch-icon.png"
-          alt="Mumble Logo (0)"
-        />
-      </span>
+      <img
+        className="main500page__logo"
+        src="https://mumble.dev/apple-touch-icon.png"
+        alt="Mumble Logo (0)"
+      />
+      <img
+        className="main500page__logo"
+        src="https://mumble.dev/apple-touch-icon.png"
+        alt="Mumble Logo (0)"
+      />
     </h1>
-    <h2>Internal Server Error !</h2>
-    <p>Something went wrong</p>
-    <br />
+    <h2 className="main500page__title">Internal Server Error !</h2>
+    <p className="main500page__info">Something went wrong</p>
     <Link to="/">Click here to go back to the 🏡 home page</Link>
-    <br />
-    <br />
   </div>
 );
 
