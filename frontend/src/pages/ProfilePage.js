7293e3f5a4f2682e98a5bd95f13d2c6b5377e81c
Cody Seibert <cseibert@flexion.us>
2021-04-18 09:22:59 -0400

    fixing build error

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import '../styles/components/ProfilePage.css';
 
