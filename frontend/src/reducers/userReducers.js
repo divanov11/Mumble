6b022bf9c5f41963a1d9b2f32c461de6cc74b5e4
Cody Seibert <cseibert@flexion.us>
2021-04-28 02:07:47 -0400

    fixing some initial state issues

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -21,13 +21,13 @@ import {
 export const userListReducer = (state = { users: [] }, action) => {
   switch (action.type) {
     case USER_LIST_REQUEST:
-      return { loading: true, users: [] };
+      return { ...state, loading: true };
 
     case USER_LIST_SUCCESS:
-      return { loading: false, users: action.payload };
+      return { ...state, loading: false, users: action.payload };
 
     case USER_LIST_FAIL:
-      return { loading: false, error: action.payload };
+      return { ...state, loading: false, error: action.payload };
 
     default:
       return state;
@@ -37,13 +37,13 @@ export const userListReducer = (state = { users: [] }, action) => {
 export const userListRecommendedReducer = (state = { users: [] }, action) => {
   switch (action.type) {
     case USER_LIST_RECOMMENDED_REQUEST:
-      return { loading: true, users: [] };
+      return { ...state, loading: true };
 
     case USER_LIST_RECOMMENDED_SUCCESS:
-      return { loading: false, users: action.payload };
+      return { ...state, loading: false, users: action.payload };
 
     case USER_LIST_RECOMMENDED_FAIL:
-      return { loading: false, error: action.payload };
+      return { ...state, loading: false, error: action.payload };
 
     default:
       return state;
