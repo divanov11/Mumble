8f3ace49a27c92d2cf55153d356ec418179dd26e
akzain <zainkamaal66885@gmail.com>
2021-05-27 10:22:30 +0300

    added the shortcut to jump to the search bar

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,9 +1,8 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import debounce from 'lodash/debounce';
 import { searchBarTyped } from '../actions/appActions';
-
 import '../styles/components/SearchBox.css';
 
 const SearchBox = () => {
@@ -27,6 +26,29 @@ const SearchBox = () => {
     pushKeyword(keyword);
   };
 
+// defining the function of the shortcut for the searh bar
+const ShortcutKey = (key, callback)=> {
+  const callbackRef = useRef(callback)
+  useEffect(() => {callbackRef.current = callback})
+
+  useEffect(() => {
+    function handle(event){
+      if (event.code === key) callbackRef.current(event)
+    }
+      document.addEventListener('keyup', handle)
+      return () => document.addEventListener('keyup', handle)
+  }, [key])
+}
+
+// Applying the functionality of the shortcut to the search bar
+ShortcutKey ("Slash",(event)=>{
+  // if event occured in an input or textarea do nothing
+  if (event.target.closest('input, textarea')) return;
+  
+  // else focus on the searchbox
+  else{document.getElementById('search-input').focus()}
+})
+
   return (
     <form onKeyUp={submitHandler} onSubmit={submitHandler} className="form" id="search-form">
       <i className="fas fa-search" id="search-icon"></i>
@@ -37,6 +59,8 @@ const SearchBox = () => {
         onChange={(e) => dispatch(searchBarTyped(e.target.value))}
         placeholder="Search Mumble"
       />
+      {}
+      <small className="shortcutText">Press <span className="shortcutKey">/</span> to jump to the searh bar</small>
     </form>
   );
 };
